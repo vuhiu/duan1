dc2f9b080c786665c2298996ba148b34654b4632
Vu Hieu <hvu6280@gmail.com>
2025-03-28 11:18:09 +0700

    sidebar

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -1,12 +1,60 @@
+<!-- filepath: c:\xampp\htdocs\duan1\admin\layouts\sidebar.php -->
+<?php
+// Lấy tên người dùng nếu đã đăng nhập
+$user_name = isset($_SESSION['user']) ? $_SESSION['user']['name'] : "Hiếu";
+?>
 
+<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">ElectroShop</span>
+    </a>
+
+    <!-- Sidebar -->
     <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block"><?= htmlspecialchars($user_name) ?></a>
+            </div>
+        </div>
+
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Tìm kiếm chức năng" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Dashboard -->
                 <li class="nav-item">
-                    <a class="nav-link">
-                        <i class="nav-icon fas fa-copy"></i>
-                        <p>Sản Phẩm<i class="fas fa-angle-left right"></i></p>
+                    <a href="index.php" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+
+                <!-- Sản Phẩm -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-box"></i>
+                        <p>
+                            Sản Phẩm
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
@@ -21,14 +69,27 @@
                                 <p>Danh sách sản phẩm</p>
                             </a>
                         </li>
+                    </ul>
+                </li>
+
+                <!-- Danh Mục -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-list"></i>
+                        <p>
+                            Danh Mục
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="http://localhost/duan1/admin/?act=danhmuc&page=add" class="nav-link">
+                            <a href="index.php?act=danhmuc&page=add" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Thêm danh mục</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="http://localhost/duan1/admin/?act=danhmuc&page=list" class="nav-link">
+                            <a href="index.php?act=danhmuc&page=list" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Danh sách danh mục</p>
                             </a>
@@ -36,14 +97,33 @@
                     </ul>
                 </li>
 
+                <!-- Bài Viết -->
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-copy"></i>
-                        <p>Bài Viết <i class="fas fa-angle-left right"></i></p>
+                        <i class="nav-icon fas fa-newspaper"></i>
+                        <p>
+                            Bài Viết
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
                     </a>
-
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="index.php?act=baiviet&page=them" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Thêm bài viết</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="index.php?act=baiviet&page=list" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Danh sách bài viết</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </nav>
+        <!-- /.sidebar-menu -->
     </div>
+    <!-- /.sidebar -->
 </aside>

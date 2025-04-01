8902740ac7f2597eb4e8fc123a36c0dc8f6cdf72
Huyen <huyen523h@gmail.com>
2025-04-02 00:02:44 +0700

    update login/logout client

--- client/views/form-register.php
+++ client/views/form-register.php
@@ -3,36 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng ký Client</title>
+    <title>Đăng ký</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light">
+<body>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <div class="card shadow">
-                    <div class="card-header text-center">
-                        <h3>Đăng ký Client</h3>
+                <h2 class="text-center mb-4">Đăng ký</h2>
+                <form action="/duan1/client/controllers/AuthController.php?action=register" method="POST" class="p-4 border rounded">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Họ và tên:</label>
+                        <input type="text" id="name" name="name" class="form-control" required>
                     </div>
-                    <div class="card-body">
-                        <form method="POST" action="/duan1/client/controllers/authenController.php?action=register">
-                            <div class="mb-3">
-                                <label for="name" class="form-label">Tên:</label>
-                                <input type="text" id="name" name="name" class="form-control" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email:</label>
-                                <input type="email" id="email" name="email" class="form-control" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Mật khẩu:</label>
-                                <input type="password" id="password" name="password" class="form-control" required>
-                            </div>
-                            <button type="submit" class="btn btn-success w-100">Đăng ký</button>
-                        </form>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" id="email" name="email" class="form-control" required>
                     </div>
-                </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Mật khẩu:</label>
+                        <input type="password" id="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Đăng ký</button>
+                </form>
+                <p class="text-center mt-3">Đã có tài khoản? <a href="/duan1/client/views/form-login.php">Đăng nhập</a></p>
             </div>
         </div>
     </div>
--- client/views/home.php
+++ client/views/home.php
@@ -1,4 +1,4 @@
-<!-- filepath: c:\xampp\htdocs\duan1\client\views\home.php -->
+<!-- filepath: c:\laragon\www\duan1\client\views\home.php -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,113 +12,120 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
 
     <!-- Bootstrap -->
-    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="/duan1/css/bootstrap.min.css" />
 
     <!-- Slick -->
-    <link type="text/css" rel="stylesheet" href="css/slick.css" />
-    <link type="text/css" rel="stylesheet" href="css/slick-theme.css" />
+    <link type="text/css" rel="stylesheet" href="/duan1/css/slick.css" />
+    <link type="text/css" rel="stylesheet" href="/duan1/css/slick-theme.css" />
 
     <!-- nouislider -->
-    <link type="text/css" rel="stylesheet" href="css/nouislider.min.css" />
+    <link type="text/css" rel="stylesheet" href="/duan1/css/nouislider.min.css" />
 
     <!-- Font Awesome Icon -->
     <link rel="stylesheet" href="css/font-awesome.min.css">
 
     <!-- Custom stylesheet -->
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <link type="text/css" rel="stylesheet" href="/duan1/css/style.css" />
 </head>
 
 <body>
+
     <!-- SECTION: New Products -->
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <!-- Section Title -->
-                <div class="col-md-12">
-                    <div class="section-title">
-                        <h3 class="title">Sản phẩm mới</h3>
-                        <div class="section-nav">
-                            <ul class="section-tab-nav tab-nav">
-                                <li class="active"><a data-toggle="tab" href="#tab1">Laptop</a></li>
-                                <li><a data-toggle="tab" href="#tab1">Điện thoại</a></li>
-                                <li><a data-toggle="tab" href="#tab1">Máy ảnh</a></li>
-                                <li><a data-toggle="tab" href="#tab1">Phụ kiện</a></li>
-                            </ul>
-                        </div>
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <!-- Section Title -->
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h3 class="title">Sản phẩm mới</h3>
+                    <div class="section-nav">
+                        <ul class="section-tab-nav tab-nav">
+                            <li class="active"><a data-toggle="tab" href="#tab1">Laptop</a></li>
+                            <li><a data-toggle="tab" href="#tab1">Điện thoại</a></li>
+                            <li><a data-toggle="tab" href="#tab1">Máy ảnh</a></li>
+                            <li><a data-toggle="tab" href="#tab1">Phụ kiện</a></li>
+                        </ul>
                     </div>
                 </div>
-                <!-- /Section Title -->
+            </div>
+            <!-- /Section Title -->
 
-                <!-- Product List -->
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="products-tabs">
-                            <div id="tab1" class="tab-pane active">
-                                <div class="products-slick" data-nav="#slick-nav-1">
-                                    <?php
-                                    // Include the ProductModel and fetch products
-                                    require_once __DIR__ . '/../models/ProductModel.php';
-                                    $productModel = new Product();
-                                    $products = $productModel->getAllProduct();
+            <!-- Product List -->
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="products-tabs">
+                        <div id="tab1" class="tab-pane active">
+                            <div class="products-slick" data-nav="#slick-nav-1">
+                                <?php
+                                // Include the ProductModel and fetch products
+                                require_once __DIR__ . '/../models/ProductModel.php';
+                                $productModel = new Product();
+                                $products = $productModel->getAllProduct();
 
-                                    // Loop through the products and display them
-                                    foreach ($products as $product): ?>
-                                        <div class="col-md-4">
-                                            <div class="product">
-                                                <div class="product-img">
-                                                    <img src="/duan1/upload/<?= htmlspecialchars($product['image']) ?>" alt="Ảnh sản phẩm">
-                                                    <div class="product-label">
-                                                        <?php if ($product['sale_price'] < $product['price']): ?>
-                                                            <span class="sale">-<?= round((($product['price'] - $product['sale_price']) / $product['price']) * 100) ?>%</span>
-                                                        <?php endif; ?>
-                                                        <span class="new">MỚI</span>
-                                                    </div>
-                                                </div>
-                                                <div class="product-body">
-                                                    <p class="product-category"><?= htmlspecialchars($product['category_name']) ?></p>
-                                                    <h3 class="product-name"><a href="#"><?= htmlspecialchars($product['name']) ?></a></h3>
-                                                    <h4 class="product-price">
-                                                        <?= number_format($product['sale_price'], 0, ',', '.') ?>₫
-                                                        <?php if ($product['sale_price'] < $product['price']): ?>
-                                                            <del class="product-old-price"><?= number_format($product['price'], 0, ',', '.') ?>₫</del>
-                                                        <?php endif; ?>
-                                                    </h4>
-                                                    <div class="product-rating">
-                                                        <i class="fa fa-star"></i>
-                                                        <i class="fa fa-star"></i>
-                                                        <i class="fa fa-star"></i>
-                                                        <i class="fa fa-star"></i>
-                                                        <i class="fa fa-star-o"></i>
-                                                    </div>
-                                                    <div class="product-btns">
-                                                        <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">Thêm vào yêu thích</span></button>
-                                                        <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">So sánh</span></button>
-                                                        <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Xem nhanh</span></button>
-                                                    </div>
-                                                </div>
-                                                <div class="add-to-cart">
-                                                    <form action="/duan1/client/views/addToCart.php" method="POST">
-                                                        <input type="hidden" name="user_id" value="<?= $_SESSION['user_id'] ?? 1; ?>"> <!-- Replace with dynamic user ID -->
-                                                        <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
-                                                        <input type="hidden" name="variant_id" value="<?= $product['variant_id'] ?? 0; ?>"> <!-- Replace with actual variant ID -->
-                                                        <input type="hidden" name="quantity" value="1">
-                                                        <button type="submit" class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> Thêm vào giỏ</button>
-                                                    </form>
+                                // Loop through the products and display them
+                                foreach ($products as $product): 
+                                    $image = htmlspecialchars($product['image'] ?? 'default.jpg');
+                                    $categoryName = htmlspecialchars($product['category_name'] ?? 'Không xác định');
+                                    $productName = htmlspecialchars($product['name'] ?? 'Không xác định');
+                                    $salePrice = $product['sale_price'] ?? 0;
+                                    $price = $product['price'] ?? 0;
+                                ?>
+                                    <div class="col-md-4">
+                                        <div class="product">
+                                            <div class="product-img">
+                                                <img src="/duan1/upload/<?= $image ?>" alt="Ảnh sản phẩm">
+                                                <div class="product-label">
+                                                    <?php if ($salePrice < $price): ?>
+                                                        <span class="sale">-<?= round((($price - $salePrice) / $price) * 100) ?>%</span>
+                                                    <?php endif; ?>
+                                                    <span class="new">MỚI</span>
                                                 </div>
                                             </div>
+                                            <div class="product-body">
+                                                <p class="product-category"><?= $categoryName ?></p>
+                                                <h3 class="product-name"><a href="#"><?= $productName ?></a></h3>
+                                                <h4 class="product-price">
+                                                    <?= number_format($salePrice, 0, ',', '.') ?>₫
+                                                    <?php if ($salePrice < $price): ?>
+                                                        <del class="product-old-price"><?= number_format($price, 0, ',', '.') ?>₫</del>
+                                                    <?php endif; ?>
+                                                </h4>
+                                                <div class="product-rating">
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star-o"></i>
+                                                </div>
+                                                <div class="product-btns">
+                                                    <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">Thêm vào yêu thích</span></button>
+                                                    <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">So sánh</span></button>
+                                                    <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Xem nhanh</span></button>
+                                                </div>
+                                            </div>
+                                            <div class="add-to-cart">
+                                                <form action="/duan1/client/views/addToCart.php" method="POST">
+                                                    <input type="hidden" name="user_id" value="<?= $_SESSION['user_id'] ?? 1; ?>">
+                                                    <input type="hidden" name="product_id" value="<?= $product['product_id'] ?>">
+                                                    <input type="hidden" name="variant_id" value="<?= $product['variant_id'] ?? 0; ?>">
+                                                    <input type="hidden" name="quantity" value="1">
+                                                    <button type="submit" class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> Thêm vào giỏ</button>
+                                                </form>
+                                            </div>
                                         </div>
-                                    <?php endforeach; ?>
-                                </div>
-                                <div id="slick-nav-1" class="products-slick-nav"></div>
+                                    </div>
+                                <?php endforeach; ?>
                             </div>
+                            <div id="slick-nav-1" class="products-slick-nav"></div>
                         </div>
                     </div>
                 </div>
-                <!-- /Product List -->
             </div>
+            <!-- /Product List -->
         </div>
     </div>
-    <!-- /SECTION: New Products -->
+</div>
+<!-- /SECTION: New Products -->
 
     <!-- SECTION: Hot Deals -->
     <div id="hot-deal" class="section">
@@ -163,36 +170,36 @@
     <!-- /SECTION: Hot Deals -->
 
     <!-- Newsletter -->
-    <div id="newsletter" class="section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="newsletter">
-                        <p>Đăng ký để nhận <strong>BẢN TIN</strong></p>
-                        <form>
-                            <input class="input" type="email" placeholder="Nhập email của bạn">
-                            <button class="newsletter-btn"><i class="fa fa-envelope"></i> Đăng ký</button>
-                        </form>
-                        <ul class="newsletter-follow">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                        </ul>
-                    </div>
+<div id="newsletter" class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="newsletter">
+                    <p>Đăng ký để nhận <strong>BẢN TIN</strong></p>
+                    <form>
+                        <input class="input" type="email" placeholder="Nhập email của bạn">
+                        <button class="newsletter-btn"><i class="fa fa-envelope"></i> Đăng ký</button>
+                    </form>
+                    <ul class="newsletter-follow">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-    <!-- /Newsletter -->
+</div>
+<!-- /Newsletter -->
 
     <!-- jQuery Plugins -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/slick.min.js"></script>
-    <script src="js/nouislider.min.js"></script>
-    <script src="js/jquery.zoom.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="/duan1/js/jquery.min.js"></script>
+    <script src="/duan1/js/bootstrap.min.js"></script>
+    <script src="/duan1/js/slick.min.js"></script>
+    <script src="/duan1/js/nouislider.min.js"></script>
+    <script src="/duan1/js/jquery.zoom.min.js"></script>
+    <script src="/duan1/js/main.js"></script>
 </body>
 
 </html>
